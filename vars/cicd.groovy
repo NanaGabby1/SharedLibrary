02379592f6f548da4b58c37d817e5e4154928f10
NanaGabby1 <105404562+NanaGabby1@users.noreply.github.com>
2022-05-27 10:38:57 -0600

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,18 +2,3 @@ def newGit(repo)
 {
   git "${repo}"
 }
-
-def newMaven()
-{
-  sh 'mvn package'
-}
-
-def newDeploy(ip,contextpath)
-{
-deploy adapters: [tomcat9(credentialsId: '3e78adb9-a385-4254-a1f1-2cda4ac89aca', path: '', url: "${ip}")], contextpath: "${contextpath}", war: '**/*.war' 
-}
-
-def runSelenium(path)
-{
-  sh "java -jar /home/ubuntu/.jenkins/workspace/"${path}/testing.jar"
-}
