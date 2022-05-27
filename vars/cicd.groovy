468f4ae7d0f9e863475bb664a230e267a29d3acb
NanaGabby1 <105404562+NanaGabby1@users.noreply.github.com>
2022-05-27 09:53:09 -0600

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,5 +10,5 @@ def newMaven()
 
 def newDeploy(ip,contextpath)
 {
-deploy adapters: [tomcat9(credentialsId: '3e78adb9-a385-4254-a1f1-2cda4ac89aca', path: '', url: ${ip})], contextpath: ${contrxtpath}, war: '**/*.war' 
+deploy adapters: [tomcat9(credentialsId: '3e78adb9-a385-4254-a1f1-2cda4ac89aca', path: '', url: '${ip}')], contextpath: '${contrxtpath}', war: '**/*.war' 
 }
